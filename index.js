c64c4f23637086fb0453e61a9d326a5e2f8e543a
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2017-10-23 14:47:18 -0700

    change the `mvdoc` help
    now
    
    ```
    usage: nx mvdoc [-h] source_document destination_folder_document
    ```
    
    before
    
    ```
    usage: nx mvdoc [-h] source_document destination_document
    ```
    
    https://www.pivotaltracker.com/story/show/133608475

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ function main() {
 
   /** mv - move nuxeo document */
   else if (args.subcommand_name === 'mvdoc') {
-    nx.mv_to_folder(client, args.source_document[0], args.destination_document[0]);
+    nx.mv_to_folder(client, args.source_document[0], args.destination_folder_document[0]);
   }
 
   // should not be possible
